b2d198512291ff526643ff5973efadb6d4372c14
SergeiKogutov <SerVik2003@gmail.com>
2022-08-22 09:32:05 +0700

    1

---
+++ Example001/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите два числа. Я скажу, какое из них больше! ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b) 
+{
+    Console.WriteLine("Первое число больше");
+}
+
+else 
+{
+    Console.WriteLine("Второе число больше");
+}
