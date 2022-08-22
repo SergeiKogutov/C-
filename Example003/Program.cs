a3cb89ed88a31e68bed596cd8e52ab7d2cde355c
SergeiKogutov <SerVik2003@gmail.com>
2022-08-22 10:26:42 +0700

    4

---
+++ Example003/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите число : ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2==0)
+{
+    Console.WriteLine("это число чётное. ");
+}
+
+else 
+{
+    Console.WriteLine("это число нечётное. ");
+}
