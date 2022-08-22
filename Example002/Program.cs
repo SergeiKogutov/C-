c8be6120facaf10f42b37ff0f0d9695589857116
SergeiKogutov <SerVik2003@gmail.com>
2022-08-22 09:55:24 +0700

    3

--- Example002/Program.cs
+++ Example002/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите три числа. Я скажу, какое из них больше! ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if (max < b) 
+{
+    max = b;
+}
+
+if (max < y)
+{
+    max = y; 
+}
+
+Console.WriteLine(max + " самое большое из трёх ");
