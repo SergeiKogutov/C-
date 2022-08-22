7f3988cd1590a9884a7caab4fb72e0ebb52ef3d6
SergeiKogutov <SerVik2003@gmail.com>
2022-08-22 15:48:42 +0700

    8

--- Example005/Program.cs
+++ Example005/Program.cs
@@ -24,20 +24,83 @@
 
 
 
-void DeleteSecondNumber1()
-{
-    Console.WriteLine("Enter number: ");
-    string number = Console.ReadLine();
+// void DeleteSecondNumber1()
+// {
+//     Console.WriteLine("Enter number: ");
+//     string number = Console.ReadLine();
     
-    for (int i = 0; i < number.Length; i++)
+//     for (int i = 0; i < number.Length; i++)
+//     {
+//         if (i != 1)
+//         {
+//             Console.Write(number[i]);
+//         }
+//     }
+//     Console.WriteLine();
+// }
+
+// DeleteSecondNumber1();
+
+// 12. Напишите программу, которая будет принимать на 
+// вход два числа и выводить, является ли второе число
+// кратным первому. Если число 2 не кратно числу 1, 
+// то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4 
+// 16, 4 -> кратно
+
+// int PrintDigit()
+// {
+//     int x = new Random() .Next(10, 100);
+//     Console.WriteLine($" случайное число сгенерировалось {x} ");
+//     int x1 = x/10;
+//     int x2 = x%10;
+//     if (x1>x2) return x1;
+//     else return x2;
+// }
+
+// Console.WriteLine (PrintDigit()*100);
+
+// 12. Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли наименьшее 
+// число кратным наибольшему. Если  наибоьшее не кратно 
+// наименьшему, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4 
+// 16, 4 -> кратно
+
+// Console.WriteLine("Введите целое число:  ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе целое число:  ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (a%b ==0) Console.WriteLine(a+ " кратно " +b);
+// else Console.WriteLine( $"{a} не кратно {b} остаток {a%b}");
+
+
+Console.WriteLine("Введите первое целое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+string Multiplicity(int num1, int num2)
+{
+    string multiplicity = String.Empty;
+    int max = num1;
+    int min = num2;
+    
+    if (num2 > num1)
     {
-        if (i != 1)
-        {
-            Console.Write(number[i]);
-        }
+        max = num2;
+        min = num1;
     }
-    Console.WriteLine();
+
+    if (max % min == 0)
+    {
+        multiplicity = $"Число {max} кратно {min}";
+    }
+    else
+    {
+        multiplicity = $"Число {max} не кратно {min}, остаток {max % min}";
+    }
+
+    return multiplicity;
 }
-
-DeleteSecondNumber1();
-
+Console.WriteLine(Multiplicity(number1, number2));
