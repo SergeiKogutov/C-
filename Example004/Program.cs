2736e5a1155b81830b498c2f8588b593edfd226f
SergeiKogutov <SerVik2003@gmail.com>
2022-08-22 10:35:58 +0700

    5

---
+++ Example004/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите число : ");
+int a = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i <= a)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+    i = i +1;
+}
