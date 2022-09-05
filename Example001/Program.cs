8cdda8cc925261d46556ad8ba9064a960060c0de
SergeiKogutov <SerVik2003@gmail.com>
2022-09-05 16:01:09 +0700

    19

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -75,3 +75,43 @@
 //     return result;
 // }
 // Console.Write(ConvertToBinary(45));
+
+// Console.WriteLine("Введите число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int c = 0;
+// string b = String.Empty;
+// while (a >= 1)
+// {
+//     c = a%2;
+//     b = b+Convert.ToString(c);
+//     a = a/2;
+// }
+// char [] b2 = b.ToCharArray();
+// Array.Reverse(b2);
+// Console.WriteLine(b2);
+
+// Задача 44: Не используя рекурсию, выведите первые N чисел 
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+
+// Console.Write("Введите количество чисел, необходимых для вывода: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int a = 0, b =1, c =0;
+
+// if (n==0) Console.Write(a);
+// else if (n == 1) Console.Write(a + " " + b);
+// else 
+// {
+//     Console.Write(a + " " + b + " ");
+//     for (int i = 2; i < n; i++)
+//     {
+//         c = a+b;
+//         Console.Write(c + " ");
+//         a =b;
+//         b =c;
+//     }
+// }
+// Console.WriteLine();
